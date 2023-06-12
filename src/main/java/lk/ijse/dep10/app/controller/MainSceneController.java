73458b31cbff1e1cdd93c523b1e3549ab5bb5b7e
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-06-12 09:23:44 +0530

    Modify the controller class

--- src/main/java/lk/ijse/dep10/app/controller/MainSceneController.java
+++ src/main/java/lk/ijse/dep10/app/controller/MainSceneController.java
@@ -1,67 +1,49 @@
 package lk.ijse.dep10.app.controller;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class MainSceneController {
 
-    @FXML
-    private Button btnCopy;
+    public Button btnCopy;
+    public Button btnDelete;
+    public Button btnMove;
+    public Button btnReset;
+    public Button btnSource;
+    public Button btnTarget;
+    public Label lblMessage;
+    public TextField txtSource;
+    public TextField txtTarget;
 
-    @FXML
-    private Button btnDelete;
 
-    @FXML
-    private Button btnMove;
-
-    @FXML
-    private Button btnReset;
-
-    @FXML
-    private Button btnSource;
-
-    @FXML
-    private Button btnTarget;
-
-    @FXML
-    private Label lblMessage;
-
-    @FXML
-    private TextField txtSource;
-
-    @FXML
-    private TextField txtTarget;
-
-    @FXML
-    void btnCopyOnAction(ActionEvent event) {
+    public void btnCopyOnAction(ActionEvent event) {
 
     }
 
-    @FXML
-    void btnDeleteOnAction(ActionEvent event) {
+
+    public void btnDeleteOnAction(ActionEvent event) {
 
     }
 
-    @FXML
-    void btnMoveOnAction(ActionEvent event) {
+
+    public void btnMoveOnAction(ActionEvent event) {
 
     }
 
-    @FXML
-    void btnResetOnAction(ActionEvent event) {
+
+    public void btnResetOnAction(ActionEvent event) {
 
     }
 
-    @FXML
-    void btnSourceOnAction(ActionEvent event) {
+
+    public void btnSourceOnAction(ActionEvent event) {
 
     }
 
-    @FXML
-    void btnTargetOnAction(ActionEvent event) {
+
+    public void btnTargetOnAction(ActionEvent event) {
 
     }
 
