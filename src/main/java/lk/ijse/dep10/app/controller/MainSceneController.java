1b7dbdaf9ae973db42732e135499eca1861d3fa4
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-06-14 10:13:40 +0530

    Implement a feature to copy files or folders

--- src/main/java/lk/ijse/dep10/app/controller/MainSceneController.java
+++ src/main/java/lk/ijse/dep10/app/controller/MainSceneController.java
@@ -1,12 +1,16 @@
 package lk.ijse.dep10.app.controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import javax.swing.*;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainSceneController {
 
@@ -25,7 +29,71 @@ public class MainSceneController {
 
 
     public void btnCopyOnAction(ActionEvent event) {
+        if (srcFile.isDirectory()) {
+            File mkFile = new File(trgtFile, srcFile.getName());
+            if (srcFile.getParentFile().getPath().equals(trgtFile.getPath())) {
+                mkFile = new File(trgtFile, "(copy)" + srcFile.getName());
+            }
+            mkFile.mkdir();
+            directoryCopy(srcFile, mkFile);
+        } else if (srcFile.isFile()) {
+            fileCopy(srcFile, trgtFile);
+        }
 
+        txtSource.clear();
+        txtTarget.clear();
+        srcFile = null;
+        trgtFile = null;
+        lblMessage.setText("Complete..!");
+        System.out.println("Complete");
+    }
+
+    private void directoryCopy(File srcFile, File trFile) {
+        File[] files = srcFile.listFiles();
+        File mkFile;
+        for (File file : files) {
+            if (file.isDirectory()) {
+                mkFile = new File(trFile, file.getName());
+                if (!mkFile.exists()) {
+                    mkFile.mkdir();
+                }
+                directoryCopy(file, mkFile);
+                continue;
+            }
+            fileCopy(file, trFile);
+        }
+    }
+
+    private void fileCopy(File file, File trFile) {
+
+        try {
+
+            File tempFile = File.createTempFile("CopY", "IteM", new File("/home/kasun/Documents/dep-10/External files/TempFiles/"));
+            FileInputStream fis = new FileInputStream(file);
+            FileOutputStream fos = new FileOutputStream(tempFile);
+
+            double write = 0.0;
+
+            while (true) {
+                byte[] buffer = new byte[1024];
+                int read = fis.read(buffer);
+                write += read;
+                if (read == -1) break;
+                fos.write(buffer, 0, read);
+            }
+
+            fis.close();
+            fos.close();
+            File target = new File(trFile, file.getName());
+            if (file.getParentFile().getPath().equals(trFile.getPath())) {
+                target = new File(trFile, "(copy)" + file.getName());
+            }
+            tempFile.renameTo(target);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Failed to copy the file, try again").show();
+        }
     }
 
 
